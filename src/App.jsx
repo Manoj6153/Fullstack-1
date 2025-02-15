1c1f0804d3367d092ce4a40b2541fca98619ee58
Manoj <manojb6153@gmail.com>
2025-02-15 09:23:59 +0530

    created homepage

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <h1>Manoj B</h1>
+        <ul>
+          <li>manojb6153@gmail.com</li>
+          <li>+91 12783678739</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
